41a6a1ca541ff2386ea802ad1327a8b7e03a0622
squid314 <squid314@users.noreply.github.com>
2016-01-30 19:46:36 -0700

    starting on option/error handling

---
+++ src/main/scala/Option.scala
@@ -0,0 +1,44 @@
+
+sealed trait Option[+A] {
+  def map[B](f: A=>B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+  def flatMap[B](f: A=>Option[B]): Option[B] =
+    map(f) getOrElse None
+  def getOrElse[B >: A](default: =>B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+  def orElse[B >: A](op: =>Option[B]): Option[B] =
+    map(Some(_)) getOrElse op
+  def filter(f: A=>Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+}
+case object None extends Option[Nothing]
+case class Some[+A](get: A) extends Option[A]
+
+object Option {
+}
+
+object OptionCode {
+  import Option._
+
+  def main(args: Array[String]) {
+    val a:Option[String] = Some("a")
+    val b:Option[String] = Some("b")
+    val n:Option[String] = None
+
+    println
+    println("map: " + a.map(_ + " map"))
+    println("getOrElse: " + a.getOrElse("asdf"))
+    println("getOrElse: " + n.getOrElse("asdf"))
+    println("flatMap: " + a.flatMap(a => if (a.contains("a")) Some("contains") else None))
+    println("flatMap: " + n.flatMap(a => if (a.contains("a")) Some("contains") else None))
+    println("filter: " + a.filter(_.contains("a")))
+    println("filter: " + a.filter(_.contains("b")))
+    println("orElse: " + a.orElse(b))
+    println("orElse: " + n.orElse(b))
+    println("orElse: " + n.orElse(n))
+  }
+}
